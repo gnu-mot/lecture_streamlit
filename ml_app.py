c8fc4dc710b35b2f0f6f2e88b0ec67dfa3288675
gnu-mot <104481995+gnu-mot@users.noreply.github.com>
2024-11-22 15:47:44 +0900

    Update ml_app.py

--- ml_app.py
+++ ml_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
